fe0ae0b5bb3a0cb42c615322a33f552c40835c83
artywhite <jeronimo2white@gmail.com>
2019-01-25 22:04:59 +0200

    Build script.

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ Chrome plugin that adds button for marking event\task as completed by applying
 strikethrough text over title.
 
 ## TODO
-- icon: 128x128, 48x48, 16x16
 - emoji icon setting (whether to put ✅ icon if task completed)
-- build script (gulp: minification + zip archive creating)
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const del = require('del');
+const gulp = require('gulp');
+const uglify = require('gulp-uglify-es').default;
+const zip = require('gulp-zip');
+const formatDate = require('date-fns/format');
+
+gulp.task('copy-icons', function() {
+  return gulp.src('./icons/*').pipe(gulp.dest('./dist/icons'));
+});
+
+gulp.task(
+  'copy',
+  gulp.series('copy-icons', function() {
+    return gulp.src(['./manifest.json']).pipe(gulp.dest('./dist'));
+  })
+);
+
+gulp.task('script', function() {
+  return gulp
+    .src('./contentScript.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('make-archive', () =>
+  gulp
+    .src('dist/**/*')
+    .pipe(zip(`gcal-checker-ext-${formatDate(new Date(), 'DD.MM.YYYY')}.zip`))
+    .pipe(gulp.dest('dist-archives/'))
+);
+
+gulp.task('clean', () => del(['dist']));
+
+gulp.task(
+  'default',
+  gulp.series('clean', gulp.series('script', 'copy', 'make-archive'))
+);
