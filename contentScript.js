5b55fba297d01216e3d58ace71a62709d5299e58
artywhite <jeronimo2white@gmail.com>
2020-10-04 00:49:56 -0700

    prettyfied

--- contentScript.js
+++ contentScript.js
@@ -1,116 +1,124 @@
-(async function(){
-  const BUTTON_ID = 'gcal-checker-chrome-ext-btn';
-  const INPUT_SELECTOR = '#xCancelBu ~ div input';
+(async function () {
+    const BUTTON_ID = 'gcal-checker-chrome-ext-btn';
+    const INPUT_SELECTOR = '#xCancelBu ~ div input';
 
-  function getInput() {
-    return new Promise((resolve, reject) => {
-      const delay = 500;
-      const maxIterations = 15;
-      let iteration = 0;
-      let timeoutLink;
+    function getInput() {
+        return new Promise((resolve, reject) => {
+            const delay = 500;
+            const maxIterations = 15;
+            let iteration = 0;
+            let timeoutLink;
 
-      let isRightPage = getIsRightPage();
-      const input = document.querySelector(INPUT_SELECTOR);
+            let isRightPage = getIsRightPage();
+            const input = document.querySelector(INPUT_SELECTOR);
 
-      if (isRightPage && input) {
-        resolve(input);
+            if (isRightPage && input) {
+                resolve(input);
 
-        return;
-      }
+                return;
+            }
 
-      function checkInDelay() {
-        timeoutLink = setTimeout(() => {
-          isRightPage = getIsRightPage();
-          const input = document.querySelector(INPUT_SELECTOR);
-          if (isRightPage && input) {
-            resolve(input);
-            clearTimeout(timeoutLink);
+            function checkInDelay() {
+                timeoutLink = setTimeout(() => {
+                    isRightPage = getIsRightPage();
+                    const input = document.querySelector(INPUT_SELECTOR);
+                    if (isRightPage && input) {
+                        resolve(input);
+                        clearTimeout(timeoutLink);
 
+                        return;
+                    }
+
+                    if (iteration > maxIterations) {
+                        reject(new Error('Input node was not found; timeout'));
+                        return;
+                    }
+
+                    iteration += 1;
+                    checkInDelay();
+                }, delay);
+            }
+
+            checkInDelay();
+        });
+    }
+
+    async function makeReplacement() {
+        const input = await getInput();
+        let nextValue = input.value;
+
+        const isAlreadyStriked = [...nextValue].some(
+            (item) => item === '\u0336'
+        );
+
+        if (isAlreadyStriked) {
+            if (nextValue[0] === '✅') {
+                nextValue = nextValue.slice(2); // emoji and space
+            }
+            nextValue = nextValue
+                .split('')
+                .filter((item) => item !== '\u0336')
+                .join('');
+        } else {
+            nextValue = nextValue
+                .split('')
+                .map((item) => item + '\u0336')
+                .join('');
+            nextValue = `✅ ${nextValue}`;
+        }
+
+        input.value = `${nextValue}`;
+
+        input.dispatchEvent(new Event('input', { bubbles: true }));
+    }
+
+    function onButtonClicked() {
+        makeReplacement();
+    }
+
+    function addButton(container) {
+        const alreadyButton = document.getElementById(BUTTON_ID);
+        if (alreadyButton) {
             return;
-          }
+        }
 
-          if (iteration > maxIterations) {
-            reject(new Error('Input node was not found; timeout'));
-            return;
-          }
+        const button = document.createElement('button');
+        button.innerText = '✅';
 
-          iteration += 1;
-          checkInDelay();
-        }, delay);
-      }
+        button.style.border = 'none';
+        button.style.outline = 'none';
+        button.style.padding = 0;
+        button.style.margin = 0;
+        button.style.background = 'none';
+        button.style.fontSize = '1.1em';
 
-      checkInDelay();
-    });
-  }
+        button.id = BUTTON_ID;
 
-  async function makeReplacement() {
-    const input = await getInput();
-    let nextValue = input.value;
+        button.addEventListener('click', onButtonClicked);
 
-    const isAlreadyStriked = [...nextValue].some(item => item === '\u0336');
-
-    if (isAlreadyStriked) {
-      if (nextValue[0] === "✅") {
-        nextValue = nextValue.slice(2); // emoji and space
-      }
-      nextValue = nextValue.split('').filter(item => item !== '\u0336').join('');
-    } else {
-      nextValue = nextValue.split('').map((item => item + '\u0336')).join("");
-      nextValue = `✅ ${nextValue}`
+        container.appendChild(button);
     }
 
-    input.value = `${nextValue}`;
-
-    input.dispatchEvent(new Event('input', { 'bubbles': true }))
-  }
-
-  function onButtonClicked() {
-    makeReplacement();
-  }
-
-  function addButton(container) {
-    const alreadyButton = document.getElementById(BUTTON_ID);
-    if (alreadyButton) {
-      return;
+    async function startAdding() {
+        try {
+            const input = await getInput();
+            addButton(input.parentElement);
+        } catch (error) {
+            // TODO
+        }
     }
 
-    const button = document.createElement('button');
-    button.innerText = '✅';
-
-    button.style.border = 'none';
-    button.style.outline = 'none';
-    button.style.padding = 0;
-    button.style.margin = 0;
-    button.style.background = 'none';
-    button.style.fontSize = '1.1em';
-
-    button.id = BUTTON_ID;
-
-    button.addEventListener('click', onButtonClicked);
-
-    container.appendChild(button);
-  }
-
-  async function startAdding() {
-    try {
-      const input = await getInput();
-      addButton(input.parentElement);
-    } catch (error) {
-      // TODO
+    function getIsRightPage() {
+        return window.location.href.indexOf('eventedit') !== -1;
     }
-  }
 
-  function getIsRightPage() {
-    return window.location.href.indexOf("eventedit") !== -1;
-  }
+    function init() {
+        startAdding();
 
-  function init() {
-    startAdding();
+        window.addEventListener('popstate', function (e) {
+            startAdding();
+        });
+    }
 
-    window.addEventListener('popstate', function(e) {
-      startAdding();
-    });
-  }
-
-  init();
-})()
+    init();
+})();
